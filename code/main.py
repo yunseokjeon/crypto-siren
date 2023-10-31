f4f6d37159b996ccf15830b4d9f3254129829604
Yun <ysjhmtb@gmail.com>
2023-10-31 18:38:28 +0900

    CCXT 예시

--- code/main.py
+++ code/main.py
@@ -1,86 +1,101 @@
-# Import the requests library
-import requests
-from jproperties import Properties
-from binance.spot import Spot
+import ccxt
+import pprint
+import pandas as pd
 
-# application.properties
-configs = Properties()
-with open('./resource/application.properties', 'rb') as config_file:
-    configs.load(config_file)
+# 현재가 조회
+binance = ccxt.binance()
+markets= binance.load_markets()
 
-secretKey = configs.get('TAAPI_KEY')
+print(markets.keys())
+print(len(markets))
 
-# Define indicator
-indicator = "candle"
-
-# Define endpoint
-endpoint = f"https://api.taapi.io/{indicator}"
-
-# Define a parameters dict for the parameters to be sent to the API
-parameters = {
-    'secret': secretKey,
-    'exchange': 'binance',
-    'symbol': 'BTC/USDT',
-    'interval': '30m',
-    'backtracks': '10'
-}
-
-# Send get request and save the response as response object
-response = requests.get(url=endpoint, params=parameters)
-
-# Extract data in json format
-result = response.json()
-
-# Print result
-print(result)
-
-# [{'timestampHuman': '2023-05-28 07:30:00 (Sunday) UTC', 'timestamp': 1685259000, 'open': 27198.73, 'high': 27217.81, 'low': 27195.4, 'close': 27201.98, 'volume': 220.25207000000034, 'backtrack': 0}, ... ]
-
-
-# Binance
-
-client = Spot()
-
-# https://binance-docs.github.io/apidocs/spot/en/#kline-candlestick-data
-
-print(client.klines("BTCUSDT", "30m"))
+btc = binance.fetch_ticker("BTC/USDT")
+pprint.pprint(btc)
 
 '''
 
-[   [1684369800000, 
-    '27406.85000000', 
-    '27420.96000000', 
-    '27354.15000000', 
-    '27357.42000000', 
-    '623.13169000', 
-    1684371599999, 
-    '17062781.06046580', 
-    17031, 
-    '326.70465000', 
-    '8946781.96350270', 
-    '0'], 
-    
-    ...]
-[
-  [
-    1499040000000,      // Kline open time
-    "0.01634790",       // Open price
-    "0.80000000",       // High price
-    "0.01575800",       // Low price
-    "0.01577100",       // Close price
-    "148976.11427815",  // Volume
-    1499644799999,      // Kline Close time
-    "2434.19055334",    // Quote asset volume
-    308,                // Number of trades
-    "1756.87402397",    // Taker buy base asset volume
-    "28.46694368",      // Taker buy quote asset volume
-    "0"                 // Unused field, ignore.
-  ]
-]
+ask	매도 1호가
+askVolume	매도 1호과 물량
+bid	매수 1호가
+bidVolume	매수 1호과 물량
+datetime	현재시간
+timestamp	타임 스탬프
+open	시가
+high	고가
+low	저가
+close	종가
+symbol	심볼
+
+{'ask': 34175.17,
+ 'askVolume': 4.33404,
+ 'average': 34190.965,
+ 'baseVolume': 35555.19182,
+ 'bid': 34175.16,
+ 'bidVolume': 0.76727,
+ 'change': -31.61,
+ 'close': 34175.16,
+ 'datetime': '2023-10-31T07:47:30.476Z',
+ 'high': 34856.0,
+ 'info': {'askPrice': '34175.17000000',
+          'askQty': '4.33404000',
+          'bidPrice': '34175.16000000',
+          'bidQty': '0.76727000',
+          'closeTime': '1698738450476',
+          'count': '1181784',
+          'firstId': '3261580425',
+          'highPrice': '34856.00000000',
+          'lastId': '3262762208',
+          'lastPrice': '34175.16000000',
+          'lastQty': '0.11934000',
+          'lowPrice': '34062.84000000',
+          'openPrice': '34206.77000000',
+          'openTime': '1698652050476',
+          'prevClosePrice': '34206.77000000',
+          'priceChange': '-31.61000000',
+          'priceChangePercent': '-0.092',
+          'quoteVolume': '1224996810.94131770',
+          'symbol': 'BTCUSDT',
+          'volume': '35555.19182000',
+          'weightedAvgPrice': '34453.38776803'},
+ 'last': 34175.16,
+ 'low': 34062.84,
+ 'open': 34206.77,
+ 'percentage': -0.092,
+ 'previousClose': 34206.77,
+ 'quoteVolume': 1224996810.9413178,
+ 'symbol': 'BTC/USDT',
+ 'timestamp': 1698738450476,
+ 'vwap': 34453.38776803}
+ 
 '''
 
+# 분봉 조회
 
-binanceKey = configs.get('BINANCE_KEY')
-binanceSecret = configs.get('BINANCE_SECRET')
+btc_ohlcv = binance.fetch_ohlcv("BTC/USDT")
+
+df = pd.DataFrame(btc_ohlcv, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
+df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
+df.set_index('datetime', inplace=True)
+print(df)
+
+# 일봉 조회
+
+btc_ohlcv = binance.fetch_ohlcv("BTC/USDT", '1d')
+
+df = pd.DataFrame(btc_ohlcv, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
+df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
+df.set_index('datetime', inplace=True)
+print(df)
+
+# 호가 조회
+
+orderbook = binance.fetch_order_book('ETH/USDT')
+print(orderbook['asks'])
+print(orderbook['bids'])
+
+
+
+
+
 
 
